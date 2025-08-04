6f3b530656167636f899a3105fbdc54dafaf2f56
Bemnet57 <bemnetasseged57@gmail.com>
2025-08-04 23:14:07 +0300

    commented on views details

--- my_tennis_club_01/members/views.py
+++ my_tennis_club_01/members/views.py
@@ -3,12 +3,12 @@ from django.template import loader
 from .models import Member
 
 def members(request):
-  mymembers = Member.objects.all().values()
-  template = loader.get_template('all_members.html')
-  context = {
-    'mymembers': mymembers,
+  mymembers = Member.objects.all().values() #Creates a mymembers object with all the values of the Member model.
+  template = loader.get_template('all_members.html')#Loads the all_members.html template.
+  context = {                 #Creates an object containing the mymembers object.
+    'mymembers': mymembers,   #Sends the object to the template.
   }
-  return HttpResponse(template.render(context, request))
+  return HttpResponse(template.render(context, request)) #Outputs the HTML that is rendered by the template.
 # def members(request):
 #   template = loader.get_template('myfirst.html')
 #   return HttpResponse(template.render())
